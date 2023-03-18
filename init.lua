755932ce9c93c44d4d6ca633784d010ea923e87f
Matt Blaisdell <blaisdellma@gmail.com>
2023-03-18 10:41:53 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,47 @@
+local function toggle_line(line)
+	if string.len(line) <= 6 then
+		return line
+	elseif string.sub(line,1,5) == " ☐ " then
+		return " ☑ " .. string.sub(line,6)
+	elseif string.sub(line,1,5) == " ☑ " then
+		return " ☐ " .. string.sub(line,6)
+	else
+		return line
+	end
+end
+
+local function toggle(aline,bline)
+	if aline == bline then
+		local line = vim.api.nvim_get_current_line()
+		vim.api.nvim_set_current_line(toggle_line(line));
+	else
+		local lines = vim.api.nvim_buf_get_lines(0,aline-1,bline,1)
+		for i,line in pairs(lines) do
+			lines[i] = toggle_line(line)
+		end
+		vim.api.nvim_buf_set_lines(0,aline-1,bline,1,lines);
+	end
+end
+
+local function makelist(aline,bline)
+	if aline == bline then
+		local line = vim.api.nvim_get_current_line()
+		vim.api.nvim_set_current_line(' ☐ ' .. line);
+	else
+		local lines = vim.api.nvim_buf_get_lines(0,aline-1,bline,1)
+		for i,line in pairs(lines) do
+			if string.len(line) ~= 0 then
+				lines[i] = ' ☐ ' .. line
+			end
+		end
+		vim.api.nvim_buf_set_lines(0,aline-1,bline,1,lines);
+	end
+end
+
+vim.api.nvim_exec("command! -range Toggle lua require'nvim-checklist-plugin'.toggle(<line1>,<line2>)", true);
+vim.api.nvim_exec("command! -range Makelist lua require'nvim-checklist-plugin'.makelist(<line1>,<line2>)", true);
+
+return {
+	toggle = toggle,
+	makelist = makelist
+}
